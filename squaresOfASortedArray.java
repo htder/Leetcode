6bb11e80d5d68179be90157363d23477b41dcc50
Henry Derbyshire <henry@htderbyshire.com>
2022-02-18 17:46:47 +0000

    Squares of a Sorted Array

--- squaresOfASortedArray.java
+++ squaresOfASortedArray.java
@@ -2,7 +2,7 @@ class Solution {
   public int[] sortedSquares(int[] nums) {
     int[] sorted = new int[nums.length];
     int start = 0;
-    int end = nums.length;
+    int end = nums.length - 1;
     for (int i = nums.length - 1; i >= 0; i--) {
       int val;
       if (Math.abs(nums[start]) > Math.abs(nums[end])) {
