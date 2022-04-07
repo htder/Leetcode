a6af109f341abc6a61c224aae359610ce335d0ab
Henry Derbyshire <henry@htderbyshire.com>
2022-04-07 19:26:37 +0100

    Completed Average Of Subarray Of Size K Challenge

---
+++ patterns_for_coding_questions/sliding_window/averageOfSubarrayOfSizeK.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class AverageOfSubarrayOfSizeK {
+  public static double[] findAverages(int K, int[] arr) {
+    double[] result = new double[arr.length - K + 1];
+    double windowSum = 0;
+    int windowStart = 0;
+    for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+      windowSum += arr[windowEnd];
+      if (windowEnd >= K - 1) {
+        result[windowStart] = windowSum / K;
+        windowSum -= arr[windowStart];
+        windowStart++;
+      }
+    }
+    return result;
+  }
+}
