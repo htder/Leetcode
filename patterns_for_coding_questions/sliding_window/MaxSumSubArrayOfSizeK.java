b9118c927a7862557ab8dfe0ddb5b91df9e61c92
Henry Derbyshire <henry@htderbyshire.com>
2022-04-07 19:38:29 +0100

    Added notes to Max Sum Subarray Of Size K

--- patterns_for_coding_questions/sliding_window/MaxSumSubArrayOfSizeK.java
+++ patterns_for_coding_questions/sliding_window/MaxSumSubArrayOfSizeK.java
@@ -2,15 +2,29 @@ import java.util.*;
 
 class MaxSumSubArrayOfSizeK {
   public static int findMaxSumSubArray(int k, int[] arr) {
+    // initialize the largest value to be the smallest
+    // possible value.
     int largest = Integer.MIN_VALUE;
+    // keep track of the current windows sum.
     int current = 0;
+    // keep track of the starting position
+    // of the current window.
     int startWindow = 0;
 
     for (int endWindow = 0; endWindow < arr.length; endWindow++) {
+      // add each value to the current window sum.
       current += arr[endWindow];
+      // if the end of the window is larger
+      // or equal to k - 1, we need to remove elements
+      // so on the next loop the "window" stays at the
+      // correct length.
       if (endWindow >= k - 1) {
+        // update largest is current is larger.
         largest = Math.max(current, largest);
+        // delete the first value in the current
+        // window.
         current -= arr[startWindow];
+        // increase the window starting position.
         startWindow++;
       }
     }
