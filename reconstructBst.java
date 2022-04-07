5537716cc2ee5d5f16f8d4d536ee1342ff711d6b
Henry Derbyshire <henry@htderbyshire.com>
2022-04-07 11:26:31 +0100

    Added notes to Reconstruct BST

--- reconstructBst.java
+++ reconstructBst.java
@@ -11,6 +11,7 @@ class Program {
     }
   }
 
+  // class to keep track of the current root node.
   static class TreeInfo {
     public int rootIdx;
 
@@ -20,6 +21,7 @@ class Program {
   }
 
   public BST reconstructBst(List<Integer> preOrderTraversalValues) {
+    // initilize root not to zero, first element in the array.
     TreeInfo treeInfo = new TreeInfo(0);
     return reconstructBstFromRange(
       Integer.MIN_VALUE,
@@ -35,22 +37,37 @@ class Program {
     List<Integer> preOrderTraversalValues,
     TreeInfo currentSubtreeInfo
   ) {
+    // if the root node is the length of the array, we know we 
+    // have traversed the entire array and can return.
     if (currentSubtreeInfo.rootIdx == preOrderTraversalValues.size()) {
       return null;
     }
 
+    // get the current value from the array.
     int rootValue = preOrderTraversalValues.get(currentSubtreeInfo.rootIdx);
+    // if the root value is not within the lower and upper 
+    // bounds, we know that we are trying to create a node
+    // in the wrong place and can return.
     if (rootValue < lowerBound || rootValue >= upperBound) {
       return null;
     }
 
+    // increment the root index from the tree info class, so that the
+    // next time this function is called we are using the next
+    // value in the array.
     currentSubtreeInfo.rootIdx++;
+    // find all the values in the left subtree.
+    // using the same lower bound but the upper bound
+    // being the root value.
     BST leftSubtree = reconstructBstFromRange(
       lowerBound,
       rootValue,
       preOrderTraversalValues,
       currentSubtreeInfo
     );
+    // find all the values in the right subtree.
+    // using the same upper bound but the root value
+    // as the lower bound.
     BST rightSubtree = reconstructBstFromRange(
       rootValue,
       upperBound,
@@ -58,7 +75,10 @@ class Program {
       currentSubtreeInfo
     );
 
+    // create the node.
     BST bst = new BST(rootValue);
+    // set the left and right nodes the be the return
+    // values from the previous to function calls above.
     bst.left = leftSubtree;
     bst.right = rightSubtree;
     return bst;
