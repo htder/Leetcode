0f73cf9001e470e3f0628396fb2951d0b549b0ea
Henry Derbyshire <henry@htderbyshire.com>
2022-06-01 16:55:23 +0100

    completed comparing string containing backspaces challenge

---
+++ patterns_for_coding_questions/two_pointers/BackspaceCompare.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class BackspaceCompare {
+  public static boolean compare(String str1, String str2) {
+    int index1 = str1.length() - 1;
+    int index2 = str2.length() - 1;
+    while (index1 >= 0 || index2 >= 0) {
+      int i1 = getNextChar(str1, index1);
+      int i2 = getNextChar(str2, index2);
+
+      if (i1 < 0 && i2 < 0) {
+        return true;
+      }
+      if (i1 < 0 || i2 < 0) {
+        return false;
+      }
+      if (str1.charAt(i1) != str2.charAt(i2)) {
+        return false;
+      }
+
+      index1 = i1 - 1;
+      index2 = i2 - 1;
+    }
+
+    return true;
+  }
+
+  private static int getNextChar(String str, int index) {
+    int backspaceCount = 0;
+    while (index >= 0) {
+      if (str.charAt(index) == '#') {
+        backspaceCount++;
+      } else if (backspaceCount > 0) {
+        backspaceCount--;
+      } else {
+        break;
+      }
+      index--;
+    }
+    return index;
+  }
+}
