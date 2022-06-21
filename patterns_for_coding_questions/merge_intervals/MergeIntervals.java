c9730ce23c72e471dd96d547a4f7b335de33ddc0
Henry Derbyshire <henry@htderbyshire.com>
2022-06-21 15:21:09 +0100

    started merge sort exercises

---
+++ patterns_for_coding_questions/merge_intervals/MergeIntervals.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Interval {
+  int start;
+  int end;
+
+  public Interval(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+}
+
+class MergeIntervals {
+  public static List<Interval> merge(List<Interval> intervals) {
+    List<Interval> mergedIntervals = new LinkedList<Interval>();
+
+
+    return mergedIntervals;
+  }
+}
