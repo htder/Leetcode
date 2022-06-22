e1ce45776d006e58bba11a3187ce90c51ce199f2
Henry Derbyshire <henry@htderbyshire.com>
2022-06-22 16:16:12 +0100

    completed merged intervals challenge

--- patterns_for_coding_questions/merge_intervals/MergeIntervals.java
+++ patterns_for_coding_questions/merge_intervals/MergeIntervals.java
@@ -13,7 +13,23 @@ class Interval {
 class MergeIntervals {
   public static List<Interval> merge(List<Interval> intervals) {
     List<Interval> mergedIntervals = new LinkedList<Interval>();
+    Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+    Iterator<Interval> intervalItr = intervals.iterator();
+    Interval interval = intervalItr.next();
+    int start = interval.start;
+    int end = interval.end;
 
+    while (intervalItr.hasNext()) {
+      interval = intervalItr.next();
+      if (interval.start <= end) {
+        end = Math.max(interval.end, end)
+      } else {
+        mergedIntervals.add(new Interval(start, end));
+        start = interval.start;
+        end = interval.end;
+      }
+    }
+    mergedIntervals.add(new Interval(start, end));
 
     return mergedIntervals;
   }
