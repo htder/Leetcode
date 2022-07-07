dbff8b706741fb19ee3b524707d95419ee14c3f3
Henry Derbyshire <henry@htderbyshire.com>
2022-07-07 12:02:40 +0100

    completed level order traversal challenge

---
+++ patterns_for_coding_questions/tree_breadth_first_search/LevelOrderTraversal.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+};
+
+class LevelOrderTraversal {
+  public static List<List<Integer>> traverse(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    if (root == null) {
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      List<Integer> currentLevel = new ArrayList<>(levelSize);
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+        currentLevel.add(currentNode.val);
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+      }
+      result.add(currentLevel);
+    }
+    return result;
+  }
+}
