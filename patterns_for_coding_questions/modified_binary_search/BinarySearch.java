073c56491eb43b4aa7debc0e9919306e81a7ed90
Henry Derbyshire <henry@htderbyshire.com>
2022-07-18 13:27:27 +0100

    completed binary search challenge

--- patterns_for_coding_questions/modified_binary_search/BinarySearch.java
+++ patterns_for_coding_questions/modified_binary_search/BinarySearch.java
@@ -1,6 +1,32 @@
 class BinarySearch {
   public static int search(int[] arr, int key) {
+    int start = 0;
+    int end = arr.length - 1;
+    int middle = start + (end - start) / 2;
+    boolean isAsc = arr[start] < arr[end];
+    if (arr[middle] == key) {
+      return middle;
+    }
 
+    while (start <= end) {
+      middle = start + (end - start) / 2;
+      if (arr[middle] == key) {
+        return middle;
+      }
+      if (isAsc) {
+        if (key < arr[middle]) {
+          end = middle - 1;
+        } else {
+          start = middle + 1;
+        }
+      } else {
+        if (key > arr[middle]) {
+          end = middle - 1;
+        } else {
+          start = middle + 1;
+        }
+      }
+    }
     return -1;
   }
 }
